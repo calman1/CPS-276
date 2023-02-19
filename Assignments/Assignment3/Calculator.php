c52e82e530e24e93f3d845b1d763f478c365878d
calman1 <calman@wccnet.edu>
2023-02-19 13:38:42 -0500

    m

--- Assignments/Assignment3/Calculator.php
+++ Assignments/Assignment3/Calculator.php
@@ -1,33 +1,71 @@
-<?php
-class Calculator{
-public function calc()
-{
+<?php 
+    
+    class Calculator{
 
-    if(func_num_args()<3)
-    {
-        return "You must enter a string and two numbers</br>";
+        function add($num1, $num2){
+            if(is_int($num1) && is_int($num2)){
+                return $num1 + $num2;
+            }
+            
         }
-        $op=func_get_arg(0);
-        $num1=func_get_arg(1);
-        $num2=func_get_arg(2);
-        if($op=="+")
-        {
-        return "The sum of the numbers is : ".($num1+$num2)."</br>";
-    }
-    if($op=="-")
-    {
-        return "The difference of the numbers : is ".($num1-$num2)."</br>";
+
+        function subtract($num1, $num2){
+            if(is_int($num1) && is_int($num2)){
+                return $num1 - $num2;
+            }
         }
-    if($op=="*")
-        {
-        return "The product of the numbers is : ".($num1*$num2)."</br>";
+
+        function multiply($num1, $num2){
+            if(is_int($num1) && is_int($num2)){
+                return $num1 * $num2;
+            }
+            
         }
-    if($op=="/")
-        {
-    if($num2!=0)
-        return "The product of the numbers is : ".($num1*$num2)."</br>";
-        else return " *** Cannot divide by zero *** <br>";
+
+        function divide($num1, $num2){
+            if(is_int($num1) && is_int($num2)){
+                return $num1 / $num2;
+                
+            }
+            
         }
-    }
-}
+
+        function calculate($ops, $num1 = "optional", $num2 = "optional"){
+
+            if(is_int($num1) && is_int($num2)){
+                
+                if($ops === '+'){
+                    
+                    $val = $this->add($num1, $num2);
+                    return "The sum of the numbers is $val .<br>";
+                    
+                }
+                else if($ops === '-'){
+                    
+                    $val = $this->subtract($num1, $num2);
+                    return "The difference of the numbers is $val .<br>";
+                }
+                else if($ops === '*'){
+                    $val = $this->multiply($num1, $num2);
+                    return "The product of the numbers is $val .<br>";
+                    
+                }
+                else if($ops === '/'){
+                    if($num2 == 0){
+                        return "Can not divide by Zero. <br>";
+                    }
+                    $val = $this->divide($num1, $num2);
+                    return "The division of the numbers is $val .<br>";
+                    
+                }
+                else{
+                    return "You must enter a string and two numbers. <br>";
+                }
+            }
+
+            return "You must enter a string and two numbers <br>";
+
+        }
+
+    }    
 ?>
