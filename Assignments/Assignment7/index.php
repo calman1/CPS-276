4396b6a695de211532a67d93f913c36b02e5dc6c
calman1 <calman@wccnet.edu>
2023-03-25 18:12:37 -0400

    update

---
+++ Assignments/Assignment7/index.php
@@ -0,0 +1,58 @@
+<?php
+if(count($_POST) > 0) {
+    require_once 'FileUpload.php';
+    $file = new FileUpload();
+    $output = $file->addFile();
+}else {
+    $output = "";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS --> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  
+    <title>Uploading Files</title>
+</head>
+<body>
+    
+<div id="wrapper" class="container">
+        <h1>File Upload</h1>
+
+    <form method="POST" action="index.php">
+
+
+    <div class ="form-row">
+
+        <p><a href = "list.php">Show File List</a></p>
+
+    </div>
+
+    <div class = "form-row">
+        <p><?php echo $output ?></p>
+    </div>
+
+    <div class="form-row">  
+        <label for="names">File Name</label>
+        <input type="text" class="form-control" name= "fileName" id="fileName">
+    </div>
+
+    <div class="form-row">
+        <input type="file" class="form-group" name="file" id="file" />
+    </div>
+
+    <div class="form-row">
+        <input type="submit" class="btn btn-primary" name="upload" value="Upload File">
+    </div>
+
+</div>
+
+
+</body>
+</html>
---
+++ Assignments/Assignment7/list.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'ListFiles.php';
+$fileList = new listFile();
+$output = $fileList->getFile();
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS --> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  
+    <title>File List</title>
+</head>
+<body>
+<div id="wrapper" class="container">
+        <h1>List Files</h1>
+
+    <form method="POST" action="list.php">
+
+
+    <div class ="form-row">
+
+        <p><a href = "index.php">Add File</a></p>
+
+    </div>
+        <?php echo $output?>
+
+</body>
+</html>
