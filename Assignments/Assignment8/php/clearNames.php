f17533ad360ffdbd3eca776c27f29dec8b0dbb9e
calman1 <calman@wccnet.edu>
2023-03-30 20:16:47 -0400

    update

--- Assignments/Assignment8/php/clearNames.php
+++ Assignments/Assignment8/php/clearNames.php
@@ -1,18 +1,18 @@
 <?php
     require '../classes/Pdo_methods.php';
     $pdo = new PdoMethods();
-    $sql = "DELETE FROM names";
+    $sql = 'DELETE FROM names';
     $result = $pdo->otherNotBinded($sql);
     if($result == 'error') {
         $response = (object) [
-            'masterstatus' => 'error',
-            'msg' => 'There was an error deleting the names',
+            'masterstatus' => "error",
+            'msg' => "There was an error deleting the names",
           ];
           echo json_encode($response);
     }else {
         $response = (object) [
-            'masterstatus' => 'success',
-            'msg' => 'Successfully deleted all names',
+            'masterstatus' => "success",
+            'msg' => "Successfully deleted all names",
           ];
           echo json_encode($response);
     }
